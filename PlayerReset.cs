4d568250799c3a7fda429448a45828c07775df12
kylbake1 <43977979+kylbake1@users.noreply.github.com>
2019-04-02 23:05:53 -0700

    Add files via upload

---
+++ PlayerReset.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerReset : MonoBehaviour
+{
+    private Vector3 startPosition; // created a vector3 called start Postion
+
+    private void Awake() // watched a tutorial and not sure what this does yet
+    {
+        startPosition = transform.position;// this will return our postion back to the beginning start postion of the game
+    }
+
+    void Start()// created void called start that will start once before update
+    {
+        
+    }
+
+    void FixedUpdate()// this is a fixed update  
+    {
+        if (Input.GetKey("space"))// beginning of if statement that will collect userinput if they press the space bar
+        {
+            transform.position = startPosition;// this will change the postion of our game object back to the beginning postion of the game
+            print("Reset position"); // this will print the following text in the console to tell us it has worked
+        }// end if
+    }//end update
+}// end class
---
+++ QuitGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    void Start() // creates a void called start that will update before Update()
+    {
+     
+    }
+    public void Update() // creates a public void called update that will update every frame
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) //if statement that will collect user input if they press the esc key
+        {// begin if
+            Application.Quit();// this will close down the application 
+            print("Has Quit Game");// this will print the following text in the console to demostate that the application has closed
+        }// end if
+    }//end void update
+}// end of class QuitGame
